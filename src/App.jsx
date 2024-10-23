350b25da12c71838a79bc05fa8b3ced807a9dcfc
thaweesap chantasathanont <thaweesap@192.168.1.35>
2024-10-23 13:02:38 +0700

    send assignment

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
+//https://platform.techupth.com/learn/courses/fsd7/react/react-react-state/assignment?courseModuleId=199&courseTopicId=960
+
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [selectedSayHi, setSelectedSayHi] = useState("Greeting Message");
+
+  const handleButtonClick = (sayHi) => {
+    setSelectedSayHi(sayHi);
+  };
+
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{selectedSayHi}</div>
       <div className="buttons">
-        <button>สวัสดี!</button>
-        <button>Hi!</button>
-        <button>你好!</button>
+        <button onClick={() => handleButtonClick("สวัสดี")}>สวัสดี!</button>
+        <button onClick={() => handleButtonClick("Hi!")}>Hi!</button>
+        <button onClick={() => handleButtonClick("你好!")}>你好!</button>
       </div>
     </div>
   );
